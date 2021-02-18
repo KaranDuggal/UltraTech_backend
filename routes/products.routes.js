614a87a7b1025940fdd3e385762ddb93fb21d34f
KaranDuggal <duggalkaran400@gmail.com>
2021-02-18 18:44:13 +0530

    add product

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/check-auth')
 
 /* GET users listing. */
 router.post('/addproduct', productController.add_product) //auth,
-router.get('/allproduct',auth, productController.get_all_product)
+router.get('/allproduct', productController.get_all_product) //auth,
 router.get('/allproduct/:id', productController.get_product)
 router.delete('/allproduct/:id/delete', productController.delete_product)
 router.put('/allproduct/:id/edit', productController.edit_product)
